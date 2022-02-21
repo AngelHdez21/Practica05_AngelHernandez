a7c668d28477e11ab7b3acd8e5ea267734de50c9
Angel de Jesus Hernandez Arenas <angel.hernandez216646@potros.itson.edu.mx>
2022-02-20 23:11:08 -0700

    Initial commit

---
+++ app/src/main/java/hernandez/angel/thecheezery/MenuActivity.kt
@@ -0,0 +1,12 @@
+package hernandez.angel.thecheezery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
